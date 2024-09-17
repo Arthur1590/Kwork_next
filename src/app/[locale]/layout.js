a5cd6f9dbc96029297d13e3f8e68b7063b90a347
Artur <iphone3034890@gmail.com>
2024-09-17 23:20:34 +0500

    main

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -13,7 +13,7 @@ const onest = Onest({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'NextFi | Home',
-	description: 'Coded by Arthur',
+	description: 'NextFi - is a crypto trading platform. Invest with confidence. Start trading now.',
 }
 
 export default async function RootLayout({ children, params: { locale } }) {
